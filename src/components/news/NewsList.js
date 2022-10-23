25765408ab7e43bd023093fe576ab4fa75943d77
ahmadfaisall.020597 <ahmadfaisall.020597@gmail.com>
2022-10-23 18:32:34 +0700

    Checkpoint Dashboard Lastnews

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,16 +1,26 @@
 import { Grid } from "@mui/material";
 import NewsCard from "./NewsCard";
+import { Box, Typography } from "@mui/material";
 
 export default function NewsList(props) {
   return (
-    <Grid container spacing={2}>
-      {props.data.map((news, index) => {
-        return (
-          <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={2} mb={5}>
-            <NewsCard key={index} news={news} />
-          </Grid>
-        );
-      })}
-    </Grid>
+    <Box>
+      <Typography
+        variant="h5"
+        fontWeight={600}
+        gutterBottom
+      >
+        Lastest News
+      </Typography>
+      <Grid container spacing={2}>
+        {props.data.map((news, index) => {
+          return (
+            <Grid key={index} item xs={12} sm={6} md={4} lg={3} xl={2} mb={5}>
+              <NewsCard key={index} news={news} />
+            </Grid>
+          );
+        })}
+      </Grid>
+    </Box>
   );
 }
